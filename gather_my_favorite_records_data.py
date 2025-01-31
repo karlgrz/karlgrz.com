1af9aff50b13d4edd4cde265e6ca713df35f30cc
Karl Grzeszczak <karl@karlgrz.com>
2025-01-31 08:36:54 -0600

    python to parse my csv of scores and filter out

--- gather_my_favorite_records_data.py
+++ gather_my_favorite_records_data.py
@@ -3,50 +3,16 @@
 import io
 import csv
 
-def format_csv_data(r):
-	si = io.StringIO()
-	cw = csv.writer(si)
-	sorted_data = sorted(r.json(), key=sort_by)
-	cw.writerow(
-		[
-			"environment",
-			"region",
-			"name",
-			"dns",
-			"id",
-			"type",
-			"platform",
-			"launchTime",
-			"imageId",
-			"imageName",
-			"imageCreationDate",
-			"lifecycle",
-			"state",
-			"flagged",
-		]
-	)
-	for row in sorted_data:
-		cw.writerow(
-			[
-				row["environment"],
-				row["region"],
-				row["name"],
-				row["dns"],
-				row["id"],
-				row["type"],
-				row["platform"],
-				row["launchTime"],
-				row["imageId"],
-				row["imageName"],
-				row["imageCreationDate"],
-				row["lifecycle"],
-				row["state"],
-				row["flagged"],
-			]
-		)
-	return si
+records = []
 
 with open('my_favorite_records_data.csv') as csvfile:
     csvreader = csv.DictReader(csvfile, delimiter=',', quotechar='"')
     for row in csvreader:
-        print(f'{row["Band"]} - {row["Album Title"]} ({row["Score (out of 10)"]} / 10)')
+        if row["Score (out of 10)"] != "N/A" and float(row["Score (out of 10)"]) >= 8:
+            records.append(row)
+
+# Sort records by score in descending order
+records.sort(key=lambda row: float(row["Score (out of 10)"]) if row["Score (out of 10)"] != "N/A" else -1, reverse=True)
+
+for row in records:
+    print(f'{row["Band"]} - {row["Album Title"]} ({row["Score (out of 10)"]} / 10) {row["Notes"]}')
